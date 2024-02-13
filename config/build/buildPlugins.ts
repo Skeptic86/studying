6d05939befb7fb174de1e6f46cd3760ec02976d7
Омаров <ruslan.omarov773@gmail.com>
2024-02-14 01:58:38 +0500

    added css modules

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,8 +1,15 @@
 import { BuildOptions } from './types/config';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 
 export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [new HtmlWebpackPlugin({ template: paths.html }), new webpack.ProgressPlugin()];
+  return [
+    new HtmlWebpackPlugin({ template: paths.html }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+  ];
 }
