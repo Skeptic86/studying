1b9227b0c463e0c0f8a60fd232c58b75e5ddfd28
Омаров <ruslan.omarov773@gmail.com>
2024-02-20 01:23:50 +0500

    added router and lazyLoading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
 import { Counter } from './components/Counter';
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 export const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={''} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
